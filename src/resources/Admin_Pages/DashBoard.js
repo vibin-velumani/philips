240ad8e65dc077ff718a51202dbbcf89b56d5f22
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-23 09:11:36 +0530

    .4

--- src/resources/Admin_Pages/DashBoard.js
+++ src/resources/Admin_Pages/DashBoard.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Cards from './Admin_Analytics/Cards'
-import BasicTable from './Admin_Analytics/Table'
 import style from '../Admin_css/dashboard.module.css'
 export default function DashBoard() {
   return (
     <div className={style.MainDash}>
     <h1>Dashboard</h1>
 
-     <Cards/>
-      <BasicTable/>
     </div>
   )
 }
